362d2b897255f262b8fafad6bc3dd27be4f3acb1
Fallrain1763 <liushuchen842@icloud.com>
2025-02-25 08:24:39 -0800

    Player Movements

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public InputAction playMovement;
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    
+    Vector2 moveDirection = Vector2.zero;
+    
+
+    private void OnEnable()
+    {
+        playMovement.Enable();
+    }
+      private void OnDisable()
+    {
+        playMovement.Disable();
+    }
+
+    void Update()
+    {
+        moveDirection = playMovement.ReadValue<Vector2>();
+        if(moveDirection == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+            if(moveDirection.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            if(moveDirection.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
+
+    
+
+}
