5d46eea536312726764c10fefacfa9a3e3931c6a
dexterdelandro <42786610+dexterdelandro@users.noreply.github.com>
2025-03-31 13:13:28 -0700

    added needs level and shop

---
+++ Assets/Shopkeeper.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shopkeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public InteractArrow arrow;
+
+  public bool inRange = false;
+   void Start()
+    {
+        arrow = GetComponentInChildren<InteractArrow>();
+        arrow.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Player")){
+            arrow.gameObject.SetActive(true);
+            collider.GetComponent<Player>().inShop = true; 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Player")){
+            arrow.gameObject.SetActive(false);
+            collider.GetComponent<Player>().inShop = false;
+        }
+    }
+}
